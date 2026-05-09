fe278a6d88db8a93f75b4169e17d7b7776332d57
programmer-professionnal <xautumx7@gmail.com>
2026-05-09 21:36:36 +0200

    feat: fármacos DB (506 entries), interacciones, firma digital, PWA manifest + service worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'samu-revorp-v1';
+const FILES_TO_CACHE = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
